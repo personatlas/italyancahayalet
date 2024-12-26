dce1632d8115c655b72481313b069294e4c332ac
Atlas Uyar <57004328+personatlas@users.noreply.github.com>
2024-12-27 01:03:06 +0300

    new

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace italyancahayalet.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
